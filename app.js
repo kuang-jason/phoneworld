40a013d08440ff0424463a376d60abba21409812
Jason Kuang <contact@jasonkuang.dev>
2019-11-04 16:21:26 -0800

    app.js variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// VARIABLES
+const productsDOM = document.querySelector(".products-center");
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+let cart = [];
