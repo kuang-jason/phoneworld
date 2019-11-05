1d7c8a125bdc593adc1c743506ae9c15ba9cfbfa
Jason Kuang <contact@jasonkuang.dev>
2019-11-05 14:20:56 -0800

    fetch products.json (class Products) and display products (class UI) implemented

--- app.js
+++ app.js
@@ -9,3 +9,49 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 let cart = [];
+
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach(product => {
+      result += `<article class="product">
+        <div class="img-container">
+        <img src=${product.image} alt="product" class="product-img"/>
+        <button class="bag-btn" data-id=${product.id}></button
+        </div>
+        <h3>${product.title}</h3>
+        <h4>${product.price}</h4>
+      </article>`;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+  // get products
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+  });
+});
